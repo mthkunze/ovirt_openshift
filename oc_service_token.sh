ea5142518d1b0ac8f33a8ded9efa2cc76dd499e6
Martin <mth.kunze@gmail.com>
2018-09-14 22:00:24 +0200

    Update oc_service_token.sh

--- oc_service_token.sh
+++ oc_service_token.sh
@@ -5,3 +5,5 @@
 oc create serviceaccount robot
 oc policy add-role-to-user admin system:serviceaccounts:test:robot
 oc serviceaccounts get-token robot
+
+oc adm policy add-cluster-role-to-user cluster-admin root
